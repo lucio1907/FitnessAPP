536a38c780a9a682037e7d1d5e2018ff9295a3dc
Lucio Gastellu Arrieta <luciogastellu.dev@gmail.com>
2024-09-19 16:05:12 -0300

    feat:frontend config done

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["300", "600", "900"] });
+
+export const metadata: Metadata = {
+  title: "FitnessAPP",
+  description: "Dedicated app for those who want to record their gym progress",
+  keywords: [
+    "gym",
+    "fitness",
+    "life",
+    "app",
+    "progress",
+    "workout",
+    "training",
+    "lifting",
+    "dumbell",
+    "exercises",
+    "life style",
+    "health",
+    "balance",
+    "motivation",
+    "personal growth"
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className}`}>{children}</body>
+    </html>
+  );
+}
